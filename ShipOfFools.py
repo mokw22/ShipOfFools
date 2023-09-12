cdd1bd86ceb2ad4f7803e5028d2122c989d50d33
Mohammed Kweder <messi.kweder01@gmail.com>
2023-09-12 15:33:01 +0200

    building the structure of the rest of classes needed for the game

--- ShipOfFools.py
+++ ShipOfFools.py
@@ -39,3 +39,58 @@ class DiceCup:
 
     def release_all(self):
         self._locked[False] * 5
+
+
+class ShipOfFools:
+    def __init__(self):
+        self._cup = DiceCup()
+        self.wining_score = 0
+
+    def turn():
+        pass
+
+
+class Player:
+    def __init__(self, name='Bot', score=0):
+        self._name = name
+        self._score = score
+
+    def current_score(self):
+        return self._score
+
+    def get_name(self):
+        return self._name
+
+    def reset_score(self):
+        self._score = 0
+
+    def play_turn(self, ShipOfFools):
+        pass
+
+
+class PlayRoom:
+    def __init__(self):
+        self._game = ShipOfFools()
+        self._players = [Player()]
+        self._winner = None
+    
+    def set_game(self, game):
+        pass
+
+    def add_player(self, player):
+        pass
+
+    def reset_scores(self):
+        pass
+
+    def play_round(self):
+        pass
+
+    def game_finished():
+        pass
+
+    def print_scores():
+        pass
+
+    def print_winner():
+        pass
