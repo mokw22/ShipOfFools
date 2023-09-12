8e50297f239ddfe5bb24bc225cd5912c838fa7bc
Mohammed Kweder <messi.kweder01@gmail.com>
2023-09-12 09:40:06 +0200

    Creating another class
    It will take 5 Die objects, take its value and check if it's banked or not

--- ShipOfFools.py
+++ ShipOfFools.py
@@ -11,3 +11,31 @@ class Die:
 
     def roll(self):
         self._value = randint(1, 6)
+
+
+class DiceCup:
+    def __init__(self):
+        self._dice = []
+        for _ in range(5):
+            self._dice.append(Die())
+        self._locked = [False] * 5
+
+    def roll(self):
+        for i in range(5):
+            if not self._locked[i]:
+                self._dice[i].roll()
+
+    def die_value(self, index):
+        return self._dice[index].get_value()
+
+    def bank(self, index):
+        self._locked[index] = True
+
+    def release(self, index):
+        self._locked[index] = False
+
+    def is_banked(self, index):
+        return self._locked[index]
+
+    def release_all(self):
+        self._locked[False] * 5
